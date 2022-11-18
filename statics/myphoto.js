85619b5376f7547030a858540b69cc68e7d75e76
Bombozaur666 <bombozaur6@gmail.com>
2022-11-18 23:21:41 +0100

    pro pagination

--- statics/myphoto.js
+++ statics/myphoto.js
@@ -10,5 +10,24 @@ $(function() {
     });
     if ($.browser.msie && $.browser.version <= 9) {
         $('#unsupported-browser-alert').show()
-    }
+    };
+
+    $('#page-1').show();
+
+    $('#services-events-pagination').bootpag({
+        total: 10
+    }).on("page", function(event, num) {
+        $('#services-events-content div').hide();
+
+        var current_page = '#page-' + num;
+        $(current_page).show();
+    });
+
+    $('[data-lp]').addClass('page-item');
+    $('.page-item > a').addClass('page-link');
+
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
 });
